ec3de7e2d030b833213d9f46e31a62a116d8ea34
fabianburghardt <sansho@xain.io>
2018-12-10 12:14:42 +0100

    added marker cam + panorama + click for kugeln

--- js/main.js
+++ js/main.js
@@ -4,3 +4,50 @@ $(document).ready(function(){
 		$("#profile-video")[0].play();
 	},1)
 })
+
+
+
+
+
+var closeObject;
+
+function zoomIn(object){
+	var material = object.attr("material");
+	console.log(material.src)
+
+	closeObject = object;
+
+	pannellum.viewer('panorama', {
+	    "type": "equirectangular",
+	    "panorama": material.src,
+	    "autoLoad": true,
+	    "showControls": false
+	});
+
+
+	object.append('<a-animation attribute="scale" dur="1500" fill="forwards" to="15 15 15" class="zoomAni"></a-animation>');
+
+	setTimeout(function(){
+		$("#panorama").fadeIn(300)
+	},1200)
+	setTimeout(function(){
+		$("#close").fadeIn(300);
+		$(".zoomAni").remove();
+	},2000)
+}
+
+
+
+$("#close").click(function(){
+	closeObject.append('<a-animation attribute="scale" dur="1600" fill="forwards" to="1 1 1"></a-animation>');
+	$("#close").fadeOut(300);
+	$("#panorama").fadeOut(300)
+})
+
+
+
+var kugel1 = $("#kugel1")[0];
+kugel1.addEventListener('click', function(evt) {
+	 zoomIn($("#kugel1"));
+});
+
